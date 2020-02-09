92d6b2bdbdebc144ec15b68c4b37f6e03302b2ff
Steven <steven@localhost.localdomain>
2020-02-09 16:54:29 -0700

    added a new function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
-int main(void) {
+#include <vector>
+
+/* An example of a range based for loop */
+void print_numbers() {
+
+	std::vector<int> my_little_list {5,15,25,35,45,55,65,75,85};
+
+	std::cout << "Printing a list of numbers inline...\n";
+	for (auto x : {10,20,30,40,50,60,70,80,90,100})	{
+		std::cout << x << '\n';
+	}
+
+	std::cout << "Printing a list of numbers that have already been defined using auto deduction...\n";
+	for (auto x : my_little_list) {
+		std::cout << x << '\n';
+	}
+}
+
+
+int main() {
 	
-	std::cout << "Hello World";
+	std::cout << "This small programs demenstrats the use of modern C++ concepts\n";
+
+	print_numbers();
 
 	return 1;
 }

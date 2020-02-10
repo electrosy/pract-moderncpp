00d6182c3444a83d557e1a77e2846843111c6f5f
Steven <steven@localhost.localdomain>
2020-02-09 17:19:01 -0700

    added lambda

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 
 #include <vector>
 
-/* An example of a range based for loop */
+/* An example of Lambda expressions */
+void lambda_test(const std::vector<int> &vn, void(*func)(int)) {
+
+	for (int v : vn) {
+		func(v);
+	}
+}
+
+/* An example of a range based for loop and Initilizations Lists */
 void print_numbers() {
 
 	std::vector<int> my_little_list {5,15,25,35,45,55,65,75,85};
@@ -24,6 +32,10 @@ int main() {
 	std::cout << "This small programs demenstrats the use of modern C++ concepts\n";
 
 	print_numbers();
+
+	std::vector<int> vn = {1,2,3,4,5,6,7,8,9,10,11,12};
+	std::cout << "Printing the values of a vector collection...\n";
+	lambda_test(vn, [](int v) {std::cout << "Value: " << v << "\n";});
 
 	return 1;
 }
